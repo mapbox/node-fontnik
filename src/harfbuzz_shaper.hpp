b6918339846b91f06477628912b83d357e19d393
Mike Morris <michael.patrick.morris@gmail.com>
2014-03-25 13:00:56 -0400

    switch harfbuzz_shaper from struct to class, ref #17

--- src/harfbuzz_shaper.hpp
+++ src/harfbuzz_shaper.hpp
@@ -38,94 +38,17 @@
 #include <harfbuzz/hb-ft.h>
 #include <harfbuzz/hb-icu.h>
 
-// typedef std::vector<FT_Glyph> glyph_vector;
-
-freetype_engine engine;
-face_manager_freetype font_manager(engine);
-
-struct harfbuzz_shaper
-{
-static void shape_text(std::string &value,
-                       std::string &fontstack,
-                       FT_Library &library)
-{
-    size_t length = value.size();
-    if (!length) return;
-
-    UnicodeString const &text = value.data();
-
-    /*
-    text_line line;
-    unsigned start = line.first_char();
-    unsigned end = line.last_char();
-
-    size_t length = end - start;
-    if (!length) return;
-    line.reserve(length);
-
-    // Preallocate memory based on estimated length.
-    // glyph_vector glyphs;
-    line.reserve(length);
-    */
-
-    auto hb_buffer_deleter = [](hb_buffer_t * buffer) { hb_buffer_destroy(buffer);};
-    const std::unique_ptr<hb_buffer_t, decltype(hb_buffer_deleter)> buffer(hb_buffer_create(),hb_buffer_deleter);
-    // hb_buffer_set_unicode_funcs(buffer.get(), hb_icu_get_unicode_funcs());
-    hb_buffer_pre_allocate(buffer.get(), length);
-
-    face_set_ptr face_set = font_manager.get_face_set(fontstack);
-    font_face_set::iterator face_itr = face_set->begin(), face_end = face_set->end();
-    for (; face_itr != face_end; ++face_itr)
-    {
-        hb_buffer_clear_contents(buffer.get());
-        std::cout<<text.length()<<" "<<length<<'\n';
-        hb_buffer_add_utf16(buffer.get(), text.getBuffer(), text.length(), 0, length);
-        // hb_buffer_set_direction(buffer.get(), (text_item.rtl == UBIDI_RTL)?HB_DIRECTION_RTL:HB_DIRECTION_LTR);
-        // hb_buffer_set_script(buffer.get(), hb_icu_script_to_script(text_item.script));
-        face_ptr const& face = *face_itr;
-        hb_font_t *font(hb_ft_font_create(face->get_face(), nullptr));
-        hb_shape(font, buffer.get(), NULL, 0);
-        hb_font_destroy(font);
-
-        unsigned num_glyphs = hb_buffer_get_length(buffer.get());
-
-        hb_glyph_info_t *glyphs = hb_buffer_get_glyph_infos(buffer.get(), nullptr);
-        hb_glyph_position_t *positions = hb_buffer_get_glyph_positions(buffer.get(), nullptr);
-
-        bool font_has_all_glyphs = true;
-        // Check if all glyphs are valid.
-        for (unsigned i=0; i<num_glyphs; ++i)
-        {
-            if (!glyphs[i].codepoint)
-            {
-                font_has_all_glyphs = false;
-                break;
-            }
-        }
-        if (!font_has_all_glyphs && face_itr+1 != face_end)
-        {
-            //Try next font in fontset
-            continue;
-        }
-
-        for (unsigned i=0; i<num_glyphs; ++i)
-        {
-            glyph_info tmp;
-            tmp.char_index = glyphs[i].cluster;
-            tmp.glyph_index = glyphs[i].codepoint;
-            tmp.face = face;
-            // tmp.format = text_item.format;
-            face->glyph_dimensions(tmp);
-            //tmp.width = positions[i].x_advance / 64.0; //Overwrite default width with better value provided by HarfBuzz
-            tmp.width = positions[i].x_advance >> 6;
-            // tmp.offset.set(positions[i].x_offset / 64.0, positions[i].y_offset / 64.0);
-            // width_map[glyphs[i].cluster] += tmp.width;
-            // line.add_glyph(tmp, scale_factor);
-        }
-        // line.update_max_char_height(face->get_char_height());
-        break; //When we reach this point the current font had all glyphs.
-    }
-}
+class harfbuzz_shaper {
+    public:
+        harfbuzz_shaper();
+        ~harfbuzz_shaper();
+        static void shape_text(std::string &value,
+                               std::string &fontstack,
+                               FT_Library &library); 
+    private:
+        freetype_engine font_engine_;
+        face_manager<freetype_engine> font_manager_;
+        std::vector<FT_Glyph> glyph_vector;
 };
 
 #endif // MAPNIK_HARFBUZZ_SHAPER_HPP
