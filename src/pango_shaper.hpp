d209fd5ae30c596bb3107f1823ce5fc8bc26def7
Mike Morris <michael.patrick.morris@gmail.com>
2014-04-04 15:48:42 -0400

    remove pango_shaper

--- src/pango_shaper.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SHAPING_HPP
-#define SHAPING_HPP
-
-#include <node.h>
-
-void InitShaping(v8::Handle<v8::Object> target);
-v8::Handle<v8::Value> Shaping(const v8::Arguments& args);
-
-#endif
