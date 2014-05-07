b95325898948137fbbdc5a2e90bd86fe375813cb
Young Hahn <young@developmentseed.org>
2014-05-07 13:17:57 -0400

    Weird env things are afoot.

--- deps/travis-build.sh
+++ deps/travis-build.sh
@@ -5,17 +5,8 @@ sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install boost1.55 gcc-4.8 g++-4.8
 
-export platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/")
-export CXX=g++-4.8
-export CC=gcc-4.8
-
 mkdir -p $BUILD
 ./deps/build_icu.sh 1>> build.log
 ./deps/build_protobuf.sh 1>> build.log
 ./deps/build_freetype.sh 1>> build.log
 ./deps/build_harfbuzz.sh 1>> build.log
-
-export PATH="$BUILD/bin:$PATH"
-echo "PKG_CONFIG_PATH is set to $PKG_CONFIG_PATH"
-ls /tmp/fontserver-build/lib/pkgconfig
-
