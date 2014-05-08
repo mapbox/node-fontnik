a38e6cad4e173a33dcbb7c2a1e68f795cd5437c6
Young Hahn <young@developmentseed.org>
2014-05-08 01:25:17 -0400

    Don't suppress output.

--- deps/travis-build.sh
+++ deps/travis-build.sh
@@ -6,7 +6,7 @@ sudo apt-get install gcc-4.8 g++-4.8
 
 mkdir -p $BUILD
 ./deps/build_icu.sh 1>> build.log
-./deps/build_boost.sh 1>> build.log
+./deps/build_boost.sh
 cat build.log
 # ./deps/build_protobuf.sh 1>> build.log
 # ./deps/build_freetype.sh 1>> build.log
