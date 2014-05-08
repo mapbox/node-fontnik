25f1fc30bd92db300d75189b01fd3384a0f4515f
Young Hahn <young@developmentseed.org>
2014-05-08 01:09:54 -0400

    Remove boost PPA/packages

--- deps/travis-build.sh
+++ deps/travis-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:boost-latest/ppa
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install boost1.55 gcc-4.8 g++-4.8
+sudo apt-get install gcc-4.8 g++-4.8
 
 mkdir -p $BUILD
 ./deps/build_icu.sh 1>> build.log
