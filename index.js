9a88b550f5abd6c97eb2fbfac3f9711de31b3b90
Young Hahn <young@mapbox.com>
2014-06-01 17:52:41 -0400

    Refactor to accept an array of character codes as input. Refs #36.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ conf(env_options);
 
 module.exports = fontserver;
 module.exports.range = range;
+module.exports.getRange = getRange;
 
 // Retrieve a range of glyphs as a pbf.
 function range(options, callback) {
@@ -21,7 +22,7 @@ function range(options, callback) {
     options.fontstack = options.fontstack || 'Open Sans Regular';
 
     var glyphs = new fontserver.Glyphs();
-    glyphs.range(options.fontstack, options.start, options.end, deflate);
+    glyphs.range(options.fontstack, options.start + '-' + options.end, getRange(options.start, options.end), deflate);
 
     function deflate(err) {
         if (err) return callback(err);
@@ -39,3 +40,15 @@ function conf(options) {
         fontserver.register_fonts(d, {recurse: true});
     });
 }
+
+function getRange(start, end) {
+    if (typeof start !== 'number') throw new Error('start must be a number from 0-65533');
+    if (start < 0) throw new Error('start must be a number from 0-65533');
+    if (typeof end !== 'number') throw new Error('end must be a number from 0-65533');
+    if (end > 65533) throw new Error('end must be a number from 0-65533');
+    if (start > end) throw new Error('start must be less than or equal to end');
+    var range = [];
+    for (var i = start; i <= end; i++) range.push(i);
+    return range;
+}
+
