85d5d615610ed40b21368fad8cd6b0febed38b08
Young Hahn <young@mapbox.com>
2015-10-16 11:16:19 -0400

    wat

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -3,10 +3,7 @@
 set -ex
 set -o pipefail
 
-if [ ! -d ~/.nvm ]; then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.28.0/install.sh | bash
-fi
-
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.28.0/install.sh | bash
 source ~/.nvm/nvm.sh
 
 nvm install ${NODE_EXE} ${NODE_VERSION}
