8eed7501058c8a2de91940aaca9cc04a2b2f5cc9
Dane Springmeyer <dane@mapbox.com>
2016-10-21 14:43:29 -0700

    gah nvm

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -16,6 +16,8 @@ nvm unload || true
 # here we set up the node version on the fly based on the matrix value.
 # This is done manually so that the build works the same on OS X
 rm -rf ./__nvm/ && git clone --depth 1 https://github.com/creationix/nvm.git ./__nvm
+set +e
+set +u
 source ./__nvm/nvm.sh
 nvm install ${NODE_VERSION}
 nvm use ${NODE_VERSION}
