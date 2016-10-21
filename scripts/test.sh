3dddcbe4f08ee2a805386268577738a4dfb517f4
Dane Springmeyer <dane@mapbox.com>
2016-10-21 15:23:26 -0700

    Fix coverage (#118)
    * exclude mason_packages from coverage reporting - refs #117
    
    * simplify nvm usage
    
    * nest sudo:false
    
    * gah nvm
    
    * need to source 'install_node'

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,8 @@
 set -e
 set -o pipefail
 
-source ~/.nvm/nvm.sh
-nvm use ${NODE_VERSION}
-
 npm test
 
 if [[ ${COVERAGE} == true ]]; then
-    cpp-coveralls --exclude node_modules --exclude tests --build-root build --gcov-options '\-lp' --exclude doc --exclude build/Debug/obj/gen
+    ${PYTHONUSERBASE}/bin/cpp-coveralls --exclude node_modules --exclude mason_packages --exclude tests --build-root build --gcov-options '\-lp' --exclude doc --exclude build/Debug/obj/gen
 fi
