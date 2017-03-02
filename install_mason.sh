e89d7982a817331e61f373dcf359a2e753765abc
Dane Springmeyer <dane@mapbox.com>
2017-03-02 11:11:15 -0800

    use mason @ https://github.com/mapbox/mason/commit/ab39e3341e051311dd03397535ee9269bf5443cf

--- install_mason.sh
+++ install_mason.sh
@@ -5,7 +5,8 @@ set -o pipefail
 
 if [ ! -d ./mason ]; then
     mkdir ./mason
-    curl -sSfL https://github.com/mapbox/mason/archive/v0.7.0.tar.gz | tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=./mason
+    # TODO use v0.8.0 tag
+    curl -sSfL https://github.com/mapbox/mason/archive/ab39e33.tar.gz | tar --gunzip --extract --strip-components=1 --exclude="*md" --exclude="test*" --directory=./mason
     ./mason/mason install boost 1.63.0
     ./mason/mason link boost 1.63.0
     ./mason/mason install freetype 2.7.1
