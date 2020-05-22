8515f08be0297797f71dcf4ddef3c7bee6c8dc80
Dane Springmeyer <dane@mapbox.com>
2020-05-22 16:19:42 -0700

    use latest clang++

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ set -eu
 set -o pipefail
 
 export MASON_RELEASE="${MASON_RELEASE:-master}"
-export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-5.0.0}"
+export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-10.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
 if [[ ${PLATFORM} == 'darwin' ]]; then
